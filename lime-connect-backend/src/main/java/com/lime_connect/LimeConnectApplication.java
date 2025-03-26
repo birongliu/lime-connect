c1eeb97b8435cbd1799e025dd567c87967a917ca
Ajani King <ajaniking16@gmail.com>
2025-03-26 09:28:44 -0400

    Initial commit

---
+++ lime-connect-backend/src/main/java/com/lime_connect/LimeConnectApplication.java
@@ -0,0 +1,13 @@
+package com.lime_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimeConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimeConnectApplication.class, args);
+    }
+
+}
